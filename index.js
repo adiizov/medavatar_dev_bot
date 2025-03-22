7a533366c2251029412e25ad188c96b1bdce1046
anvar <anvaradizov100@gmail.com>
2025-03-22 23:45:04 +0500

    menu button

--- index.js
+++ index.js
@@ -1,12 +1,35 @@
 require('dotenv').config();
-const {Bot} = require("grammy")
+const {Bot, GrammyError, HttpError, InlineKeyboard} = require("grammy")
 
 const bot = new Bot(process.env.BOT_API_KEY)
+const webAppUrl = process.env.WEB_APP_URL;
+
 
 bot.command("start", async (ctx) => {
-    await ctx.reply("Добро пожаловать!")
+    const inlineKeyboard = new InlineKeyboard().webApp("Telegram Web App", webAppUrl)
+    await ctx.reply("Добро пожаловать!", {reply_markup: inlineKeyboard})
 })
 
-bot.on("message:text", (ctx) => ctx.reply(ctx.message.text));
+bot.api.setChatMenuButton({
+    menu_button: {
+        type: "web_app",
+        text: "Открыть приложение",
+        web_app: { url: webAppUrl }
+    }
+}).catch(console.error);
+
+
+bot.catch((err) => {
+    const ctx = err.ctx
+    console.error(`error while handling update ${ctx.update.update_id}`)
+    const e = err.error
+    if(e instanceof GrammyError) {
+        console.error("error in request", e.description)
+    } else if(e instanceof HttpError) {
+        console.error("Could not contact Telegram", e)
+    } else {
+        console.error("unknown error", e)
+    }
+})
 
 bot.start()
